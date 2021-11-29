a90b9900d58d48ff2ccef3739d6c79f2131273ee
BonnyWang <bonny.y.gardy@gmail.com>
2021-11-28 19:23:54 -0500

    Added more queries

--- PS2/queries.py
+++ PS2/queries.py
@@ -13,19 +13,59 @@ def findWithRating(rate):
 
 def findNumberWithRating(rate):
     myquery = { "rating": rate};
-    return collection.find(myquery).count();
+    return collection.count_documents(myquery);
 
 def total():
     return collection.count_documents({});
 
-# TODO: Use Aggregate pipeline
+def checkInstock(name):
+    return collection.find({'title':name},{"instock availability":1, "_id":0})
+
+# Use aggregate pipe line to obtain the book with lowest price with 5 stars
+def lowestPrice5Star():
+    pipeline = [
+        {
+            "$match": {
+                "rating": "Five"
+            }
+        },
+        {
+            "$sort": {
+                "price": pymongo.ASCENDING
+            }
+        },
+        {
+            "$limit": 1
+        },
+        {
+            "$project":{
+                'title': 1,
+                'price': 1,
+                '_id': 0
+            }
+        }
+    ]
+
+    return collection.aggregate(pipeline);
 
 
 if __name__ == "__main__":
-    print("Total number of books in the database:");
+    print("Query Results:");
+    
+    print("\nTotal number of books in the database:");
     print(total());
-    # print("Books have 4 star rating:");
+    
+    print("\n\nBooks have 4 star rating:");
     books4Star = findWithRating("Four");
     for b in books4Star:
         print(b['title']);
+
+    print("\n\nNumber of books with 4-star rating:");
+    print(findNumberWithRating("Four"));
+
+    print("\n\nLowest price book with five stars:");
+    print(list(lowestPrice5Star()));
+
+    checkName = input("\n\nEnter the book name to check if it is available:");
+    print(list(checkInstock(checkName))[0]['instock availability']);
 
